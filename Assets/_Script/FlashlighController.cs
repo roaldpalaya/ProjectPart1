b2832bb40b306b90964f279ea5fe2782eb12ae12
roaldpalaya <roaldrussel@hotmail.com>
2016-12-03 05:03:31 -0500

    changed map, added instruction scene, improved flashlight

--- Assets/_Script/FlashlighController.cs
+++ Assets/_Script/FlashlighController.cs
@@ -4,27 +4,42 @@ using System.Collections;
 public class FlashlighController : MonoBehaviour {
 
     public Light flashlight;
-    public AudioSource pickup;
+    
+   
+    private bool _isActive;
     // Use this for initialization
     void Start () {
         
-	}
+        this._isActive = true;
+        flashlight.enabled = true;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
-    void OnTriggerEnter(Collider other)
-    {
+       
+            Debug.Log("key can be pressed");
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                
+                switch (this._isActive)
+                {
+                    case true:
+                        flashlight.enabled = false;
+                        this._isActive = false;
+                        break;
+                    case false:
+                        flashlight.enabled = true;
+                        this._isActive = true;
+                        break;
+                    default:
+                        break;
 
-        if (other.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("flashlight hit");
+                }
+                Debug.Log("active? : " + this._isActive);
+
+           }
             
-            Destroy(this.gameObject);
-            flashlight.enabled = true;
-
-            this.pickup.Play();
-        }
-    }
+        
+	}
+    
 }
