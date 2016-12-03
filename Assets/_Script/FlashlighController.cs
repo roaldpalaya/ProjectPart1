42974efbd5938018049837f849f1f01c378b6f58
roaldpalaya <roaldrussel@hotmail.com>
2016-12-03 00:04:41 -0500

    added flashlight

---
+++ Assets/_Script/FlashlighController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlashlighController : MonoBehaviour {
+
+    public Light flashlight;
+    public AudioSource pickup;
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("flashlight hit");
+            
+            Destroy(this.gameObject);
+            flashlight.enabled = true;
+
+            this.pickup.Play();
+        }
+    }
+}
